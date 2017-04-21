9e9201b69f17a56bd9b530b34f657d591db0b0e5
avilavate <avilavate123@gmail.com>
2017-04-21 15:41:19 +0530

    Some fixes for cyclic graphs

--- main.ts
+++ main.ts
@@ -3,18 +3,17 @@ import { DFS } from './dfs';
 
 let graphObj = new Graph([], []);
 let vSet: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8];
-let eSet: Edge = { v1: 2, v2: 3 };
 
 vSet.forEach(ele => {
     graphObj.addVirtex(ele);
 })
 
-graphObj.addEdge(eSet);
-graphObj.addEdge({ v1: 2, v2: 7 });
-graphObj.addEdge({ v1: 2, v2: 5 });
-graphObj.addEdge({ v1: 5, v2: 6 });
-graphObj.addEdge({ v1: 6, v2: 1 });
-graphObj.addEdge({ v1: 2, v2: 1 });
+graphObj.addEdge({ v1: 1, v2: 2 });
+graphObj.addEdge({ v1: 1, v2: 5 });
+graphObj.addEdge({ v1: 1, v2: 4 });
+graphObj.addEdge({ v1: 4, v2: 3 });
+graphObj.addEdge({ v1: 3, v2: 2 });
+graphObj.addEdge({ v1: 1, v2: 3 });
 
 //Disconnected edge (from'2')
 
@@ -22,4 +21,4 @@ console.log(graphObj.getTotalEdges());
 console.log(graphObj.getTotalVirtices());
 console.log(graphObj.getAllNeighbours(2));
 
-DFS(graphObj, 2);
+DFS(graphObj, 1);
